01c885a80919cd386feeaa73ba1919d63604e90a
Ernest <ernest.cheruiyot@student.moringaschool.com>
2021-09-06 12:24:05 +0300

    remove Century of Birth

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 const checkAkan = (e) => {
     e.preventDefault();
 
-const form=document.querySelector("form");
+const form=document.querySelector("#formAKAN");
 const mNames = ["kwasi","kwadwo","kwabena","kwaku","Yaw","kofi","kwame"];
 const fNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 
 
-const fd = new FormData(frm)
+const fd = new FormData(formAkan)
 const date = new date("Date of Birth")
 
 
 console.log(fNames[e])
-console.log(mNames[e])
+console.log(mNames[e]);
 
 
 
-}
-document.querySelector("form").addEventListener("submit",checkAkan);
+
+document.querySelector("#formAKAN").addEventListener("submit","checkAKAN")};
