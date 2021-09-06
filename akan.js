3b61e5f42bd14398cdc5cb98a4c1f48e671dd612
Ernest <ernest.cheruiyot@student.moringaschool.com>
2021-09-06 09:56:55 +0300

    add gh-pages

---
+++ akan.js
@@ -0,0 +1,19 @@
+const checkAkan = (e) => {
+    e.preventDefault();
+
+const form=document.querySelector("form");
+const mNames = ["kwasi","kwadwo","kwabena","kwaku","Yaw","kofi","kwame"];
+const fNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+
+
+const fd = new FormData(frm)
+const date = new date("Date of Birth")
+
+
+console.log(fNames[e])
+console.log(mNames[e])
+
+
+
+}
+document.querySelector("form").addEventListener("submit",checkAkan);
